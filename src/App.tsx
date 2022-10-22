b74ebb19cc109a9214bc7d5698290240cea4fa70
elizabeth <yelyzaveta.k@develops.today>
2022-10-22 22:11:34 +0300

    configure ESLint, Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Calendar To Do
-    </div>
-  );
+  return <div className='App'>Calendar To Do</div>;
 }
 
 export default App;
