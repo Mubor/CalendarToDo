425012756304386a232f537b52484768f1df6b11
elizabeth <yelyzaveta.k@develops.today>
2022-10-25 23:58:37 +0300

    feat: create Calendar Board component and add it to Main Page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,22 @@
+import React, { FC, useState } from 'react';
+import CalendarBoard from '../components/CalendarBoard';
+import { getMonthMatrix } from '../utils';
+import styled from 'styled-components';
+
+const MainPage: FC = (): JSX.Element => {
+  const [currentMonth, setCurrentMonth] = useState(getMonthMatrix());
+
+  return (
+    <MainPageWrapper>
+      <CalendarBoard monthMatrix={currentMonth} />
+    </MainPageWrapper>
+  );
+};
+
+const MainPageWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export default MainPage;
