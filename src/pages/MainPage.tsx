07cc16a2d339de6809fd7a2cb46dba197c8f3a7e
elizabeth <yelyzaveta.k@develops.today>
2022-10-26 23:23:34 +0300

    fix: install react-live-clock package

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,12 +2,21 @@ import React, { FC, useState, useEffect } from 'react';
 import CalendarBoard from '../components/CalendarBoard';
 import { getMonthMatrix } from '../utils';
 import styled from 'styled-components';
+import type { RootState } from '../state/store';
+import { useSelector } from 'react-redux';
+import SwitchMonth from '../components/SwitchMonth';
 
 const MainPage: FC = (): JSX.Element => {
   const [currentMonth, setCurrentMonth] = useState(getMonthMatrix());
+  const currentMonthIndex = useSelector((state: RootState) => state.currentMonth);
+
+  useEffect(() => {
+    setCurrentMonth(getMonthMatrix(currentMonthIndex));
+  }, [currentMonthIndex]);
 
   return (
     <MainPageWrapper>
+      <SwitchMonth />
       <CalendarBoard monthMatrix={currentMonth} />
     </MainPageWrapper>
   );
