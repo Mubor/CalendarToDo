9ddf2c98222376d4b9fe71badc9fad1e9adaff0c
Mubor <muhatasov22@ukr.net>
2022-10-31 12:26:00 +0200

    fix: sign in server func

--- server/src/commands/SignInCommand.ts
+++ server/src/commands/SignInCommand.ts
@@ -4,20 +4,20 @@ import { Tasks } from '../domain/models/tasks.js';
 import { DatabaseRepository } from '../ports/db-repository.js';
 
 
-export class SignUpCommand {
+export class SignInCommand {
 	constructor(private readonly dbRepository: DatabaseRepository) {}
 
-	async execute(user: UserData): Promise<void|Error> {
+	async execute(user: UserData): Promise<Tasks|Error> {
 
-		const record =  await this.dbRepository.findOne({login: user.login}, 'users')
+		const record =  await this.dbRepository.findOne({login: user.login, password: user.password}, 'users')
 
-		if(!record){
+		if(record){
 			await this.dbRepository.save(user, 'users');
 			await this.dbRepository.save(Tasks.create(user), 'tasks');
 			return;
 		}
 		else {
-			return new Error('User already exists');
+			return new Error('Login or password wa s incorrect');
 		}
 
 	}
