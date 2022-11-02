85818e75194389fbd2fbf52e71cba1222a9db6bf
elizabeth <yelyzaveta.k@develops.today>
2022-11-03 01:22:21 +0200

    feat: add new colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,29 @@
 import { ThemeType } from '../types/styles';
 
+// previous primary font color is '#EDEDED' and secondary bg color is '#444444'
 export const baseTheme: ThemeType = {
   colors: {
     bg: {
       primary: '#171717',
-      secondary: '#444444',
+      secondary: '#262626',
     },
 
     font: {
-      primary: '#EDEDED',
-      secondary: '#7E7D7D',
-      error: '#cb5d5d',
+      primary: '#7E7D7D',
+      secondary: '#C7C7C7',
+      brightLight: '#EDEDED',
+      error: '#CB5D5D',
     },
 
-    button: '#3f9ae1',
+    button: {
+      primary: '#3f9AE1',
+    },
+
+    border: {
+      primary: '#444444',
+    },
+
+    accept: '#32965C',
   },
 
   media: {
