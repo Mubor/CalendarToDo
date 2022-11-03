fc4369005c74b3f9bd03e497157f3c626e01d49d
elizabeth <yelyzaveta.k@develops.today>
2022-11-03 03:00:46 +0200

    refactor: move routes pathnames to a separate variable

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import { RoutesTypes } from '../types/routes';
+
+export const routes: RoutesTypes = {
+  calendar: '/calendar',
+  tasks: '/tasks',
+  creation: '/creation',
+  logIn: '/login',
+  signUp: '/signup',
+};
